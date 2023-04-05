f6ba47cad74c575d5108a30a9ac5ddb7e300df32
miralaba <miralaba@gmail.com>
2023-04-05 09:18:24 +0100

    starting cost-benefit analysis code

---
+++ scripts/cost_benefit_analysis.R
@@ -0,0 +1,18 @@
+
+#' @title Cost-effectiveness of conservation actions in Amazon
+#' @description 
+
+#### pre-setting ####
+memory.limit(1000000)
+
+#### loading required packages ####
+library(tidyverse)
+library(raster)
+library(rgdal)
+library(rgeos)
+library(sf)
+library(scales)
+library(reshape)
+
+
+forestdep.spplist <- read.csv("data/species_summary_final.csv")
