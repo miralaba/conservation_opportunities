36be16c2d90890be9d49ddd7b82ada6ac081866e
miralaba <miralaba@gmail.com>
2023-09-19 16:33:30 +0100

    script to build environmental layer for paragominas-pa, brasil. for more details on inputs, sources and layers built see comments in the code

--- scripts/layer_buide_pgm.R
+++ scripts/layer_buide_pgm.R
@@ -3291,9 +3291,9 @@ rm(list=ls())
 
 #######################################################################################################################
 
-# passive restoration cost
-# source:
-# pg. xx: ""
+# passive restoration cost: average per hectare cost associated with implementing natural regeneration with fence
+# 344.07 ± $ 156 US$/ha -- US$1.00 ~ R$3.87 -- R$1331.5
+# source: Bracalion et al. 2019 https://doi.org/10.1016/j.biocon.2019.108274
 
 pgm.all.forest.2010 <- raster("rasters/PGM/all_forest_mask/PGM_2010_real.tif")
 pgm.all.forest.2020.restor <- raster("rasters/PGM/all_forest_mask/PGM_2020_restor_wo_avoid.tif")
